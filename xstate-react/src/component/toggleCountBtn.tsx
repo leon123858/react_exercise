960d7ae6a95a2cb0a919fd7c9d413baacc765f3f
leon123858 <a0970785699@gmail.com>
2022-06-17 11:35:26 +0800

    add selector sample

--- xstate-react/src/component/toggleCountBtn.tsx
+++ xstate-react/src/component/toggleCountBtn.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { useContext } from 'react';
 import toggleMachine from '../machine/toggle';
-import { useMachine, useActor } from '@xstate/react';
+import { useMachine, useActor, useSelector } from '@xstate/react';
 import { GlobalStateContext } from './globalProvider';
+import loginSelector from '../utils/selector/loginSelector';
 
 const ToggleCountBtn = () => {
 	const [current, send] = useMachine(toggleMachine);
@@ -10,7 +11,11 @@ const ToggleCountBtn = () => {
 	const { count } = current.context;
 	// auth by global
 	const globalServices = useContext(GlobalStateContext);
+	// render when all auth change
 	const [authState, setAuth] = useActor(globalServices.authService);
+	// // render when auth state change (效能佳,當被別人 render 時可用, 減少無意義渲染)
+	// const isLogin = useSelector(globalServices.authService, loginSelector);
+	// console.log(isLogin);
 
 	return (
 		<React.Fragment>
